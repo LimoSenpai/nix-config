7f005572652e726902247c4ea042129c9f5224c8
Tinus Braun <brauntinus@gnetic.pro>
2025-09-08 18:32:55 +0200

    thinkcentre updates

--- hosts/thinkcentre/configuration.nix
+++ hosts/thinkcentre/configuration.nix
@@ -210,6 +210,7 @@
     "ark"
   ];
   nixos-apps-gui.extraPackages = [
+    pkgs.wdisplays
   ];
 
   #=============================================================================#
@@ -272,6 +273,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 
 }
